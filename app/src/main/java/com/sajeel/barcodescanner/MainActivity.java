d8aa3c07c1da7e66aa04fcad95d9d7809cba51f2
M-Sajeel <muhammadsajeel444@gmail.com>
2018-10-12 01:56:47 +0500

    Initial commit for Barcode Scanner

---
+++ app/src/main/java/com/sajeel/barcodescanner/MainActivity.java
@@ -0,0 +1,32 @@
+package com.sajeel.barcodescanner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static TextView tvResult;
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvResult = (TextView) findViewById(R.id.tvResult);
+
+        btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScanActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
